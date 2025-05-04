4a742fabacb98a18473a468711933f58d7531348
kajalgupta-08 <kajalgupta7997@gmail.com>
2025-05-04 21:43:59 +0530

    Update product_details.dart

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -13,17 +13,16 @@ class ProductDetails extends StatefulWidget {
 
 class _ProductDetailsState extends State<ProductDetails> {
   String selectedSize = ''; 
-
   void onTap() {
     if (selectedSize.isNotEmpty) {
-     
+      
       Provider.of<CartProvider>(context, listen: false).addproduct({
         'id': widget.product['id'],
         'title': widget.product['title'],
         'price': widget.product['price'],
         'imageUrl': widget.product['imageUrl'],
         'company': widget.product['company'],
-        'sizes': selectedSize, 
+        'sizes': selectedSize,
       });
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
@@ -40,97 +39,102 @@ class _ProductDetailsState extends State<ProductDetails> {
       );
     }
   }
-
-  @override
+@override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: Text('Details')),
-      body: Column(
-        children: [
-          Text(
-            widget.product['title'] as String,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-          ),
-          const Spacer(),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              width: 360,
-
-              child: Image.asset(
-                widget.product['imageUrl'] as String,
-                height: 400,
-                fit: BoxFit.fill,
-              ),
-            ),
-          ),
-          const Spacer(),
-          Container(
-            width: double.infinity,
-            height: 250,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(40),
-              color: Color.fromRGBO(200, 235, 247, 1),
-            ),
+      body: SingleChildScrollView(  
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),  
+          child: Center(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Text(
-                  'Rs ${widget.product['price']}',
+                  widget.product['title'] as String,
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
                 ),
-                SizedBox(height: 20),
-                SizedBox(
-                  height: 50,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: (widget.product['sizes'] as List<String>).length,
-                    itemBuilder: (context, index) {
-                      final size =
-                          (widget.product['sizes'] as List<String>)[index];
-
-                      return Padding(
-                        
-                        padding: const EdgeInsets.all(8.0),
-                        child: GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              selectedSize =
-                                  size; 
-                            });
-                          },
-                          child: Chip(
-                            label: Text(size),
-                            backgroundColor:
-                                selectedSize == size
-                                    ? Theme.of(context).colorScheme.primary
-                                    : Colors.white,
-                          ),
-                        ),
-                      );
-                    },
+                const SizedBox(height: 20), 
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: 360,
+                    child: Image.asset(
+                      widget.product['imageUrl'] as String,
+                      height: 400,
+                      fit: BoxFit.fill,
+                    ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: ElevatedButton.icon(
-                    onPressed: onTap,
-                    icon: Icon(Icons.shopping_cart_outlined),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Theme.of(context).colorScheme.primary,
-                      minimumSize: const Size(double.infinity, 50),
-                      iconColor: Colors.black,
-                      iconSize: 30,
-                    ),
-                    label: Text(
-                      "Add to cart",
-                      style: TextStyle(fontSize: 25, color: Colors.black),
+                const SizedBox(height: 20), 
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: Color.fromRGBO(200, 235, 247, 1),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Rs ${widget.product['price']}',
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                        ),
+                        SizedBox(height: 20),
+                        SizedBox(
+                          height: 50,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: (widget.product['sizes'] as List<String>).length,
+                            itemBuilder: (context, index) {
+                              final size =
+                                  (widget.product['sizes'] as List<String>)[index];
+            
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      selectedSize = size; 
+                                    });
+                                  },
+                                  child: Chip(
+                                    label: Text(size),
+                                    backgroundColor: selectedSize == size
+                                        ? Theme.of(context).colorScheme.primary
+                                        : Colors.white,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: ElevatedButton.icon(
+                            onPressed: onTap,
+                            icon: Icon(Icons.shopping_cart_outlined),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).colorScheme.primary,
+                              minimumSize: const Size(double.infinity, 50),
+                              iconColor: Colors.black,
+                              iconSize: 30,
+                            ),
+                            label: Text(
+                              "Add to cart",
+                              style: TextStyle(fontSize: 25, color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
